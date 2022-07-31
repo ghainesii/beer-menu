c6297737a64b17bfe99e7775d67e45a661c1b832
ghainesii <46608360+ghainesii@users.noreply.github.com>
2022-07-31 13:14:34 -0500

    Initial project creation (#1)

---
+++ src/main/java/net/ghaines/beer/menu/model/UntappdBeer.java
@@ -0,0 +1,3 @@
+package net.ghaines.beer.menu.model;
+
+public record UntappdBeer(String beer_name, String beer_style) { }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into On_Tap (beer, abv, brewer, city, state, style, logo, start_da) values ('Dank Meme', 5.4, 'Triptych', 'Savoy', 'IL', 'Pale Ale', null, CURRENT_TIMESTAMP);
+insert into On_Tap (beer, abv, brewer, city, state, style, logo, start_da) values ('Extended Jam', 6.8,  'Destihl', 'Bloomington', 'IL', 'IPA - Hazy', null, CURRENT_TIMESTAMP - 7);
+insert into On_Tap (beer, abv, brewer, city, state, style, logo, start_da) values ('Rhiannon''s Red', 6.4, 'Golden Fox', 'Decatur', 'IL', 'Red Ale - Irish', null, CURRENT_TIMESTAMP - 8);
+insert into On_Tap (beer, abv, brewer, city, state, style, logo, start_da) values ('D-Town Brown', 7.6, 'Decatur Brew Works', 'Decatur', 'IL', 'Brown Ale - American', null, CURRENT_TIMESTAMP - 8);
+insert into On_Tap (beer, abv, brewer, city, state, style, logo, start_da) values ('Pale Ale', 5.6, 'Sierra Nevada', 'Chico', 'CA', 'Pale Ale', null, CURRENT_TIMESTAMP - 8);
+insert into On_Tap (beer, abv, brewer, city, state, style, logo, start_da) values ('A Little Sumpin'' Sumpin'' Ale', 7.5, 'Lagunitas', 'Petaluma', 'CA', 'American Pale Wheat', null, CURRENT_TIMESTAMP - 8);
+
